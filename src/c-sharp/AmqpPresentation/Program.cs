164a4cd09dff3ef9a455074224c44258217e1778
xeb <xebxeb@gmail.com>
2012-03-03 21:54:21 -0800

    Adding C-Sharp examples

--- src/c-sharp/AmqpPresentation/Program.cs
+++ src/c-sharp/AmqpPresentation/Program.cs
@@ -1,14 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
 
 namespace AmqpPresentation
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private const string _broker = "localhost";
+
+        public static void Main(string[] args)
         {
+            Console.WriteLine("Connecting to {0}...", _broker);
+            var connectionFactory = new ConnectionFactory
+            {
+                Endpoint = new AmqpTcpEndpoint(_broker),
+                Protocol = Protocols.FromEnvironment(),
+                Port = AmqpTcpEndpoint.UseDefaultPort,       
+            };
+
+            var connection = connectionFactory.CreateConnection();
+
+            HelloWorldExample.Exec(connection);
+
+            var bindingExample = new BindingExample(connection);
+            bindingExample.Publish();
+            bindingExample.Consume();
+            // Just keep running for awhile...
+            Thread.Sleep(500000);
+
+            connection.Close();
         }
     }
 }
